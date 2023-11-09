b30577d5d068b06beb2433f90c81b0bbcaa53ff4
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2023-11-09 20:33:59 +0100

    add interpreter

--- lib/cowRoll/interpreter/interpreter.ex
+++ lib/cowRoll/interpreter/interpreter.ex
@@ -2,12 +2,43 @@ defmodule Interpreter do
   use Parser
   use DiceRoller
 
+  @type expr_ast ::
+          {:mult, aterm, aterm}
+          | {:divi, aterm, aterm}
+          | {:plus, aterm, aterm}
+          | {:minus, aterm, aterm}
+          | aterm
+
+  @type aterm ::
+          {:number, any(), integer()}
+          | {:dice, charlist()}
+          | expr_ast
+
   @spec eval_input(any()) :: any()
   def eval_input(input) do
     {:ok, ast} = Parser.parse(input)
     eval(ast)
   end
 
-  # defp eval(ast) when is_list(ast), do: Enum.reduce(ast, {0, 0}, &eval/2)
-  defp eval({:dice, dice}), do: {roll_dice(dice)}
+  @spec eval({:dice, any()} | {:number, any()}) :: any()
+  def eval({:number, number}), do: number
+
+  def eval({:dice, dice}) do
+    case(roll_dice(dice)) do
+      {:ok, dice} -> dice
+      {:error, error} -> throw({:error, error})
+    end
+  end
+
+  def eval({:mult, expresion_izquierda, expresion_derecha}),
+    do: eval(expresion_izquierda) * eval(expresion_derecha)
+
+  def eval({:plus, expresion_izquierda, expresion_derecha}),
+    do: eval(expresion_izquierda) + eval(expresion_derecha)
+
+  def eval({:minus, expresion_izquierda, expresion_derecha}),
+    do: eval(expresion_izquierda) - eval(expresion_derecha)
+
+  def eval({:divi, expresion_izquierda, expresion_derecha}),
+    do: div(eval(expresion_izquierda), eval(expresion_derecha))
 end
--- test/cowRoll/Interpreter/interpreter_test.exs
+++ test/cowRoll/Interpreter/interpreter_test.exs
@@ -3,15 +3,69 @@ defmodule CowRoll.InterpreterTest do
   use ExUnit.Case
 
   describe "string/1" do
-    test "returns :ok when attackRoll is defined" do
+    test "returns a rolled dice" do
       # Uso del analizador léxico en otro módulo
-      input = "1d6"
-
       for _ <- 1..100 do
-        {{:ok, dice}} = Interpreter.eval_input(input)
+        dice = Interpreter.eval_input("1d6")
+        assert is_integer(dice)
 
-        assert dice > 0 && dice <= 6
+        assert dice > 0 and dice <= 6
       end
+    end
+
+    test "returns a rolled dice plus 3" do
+      # Uso del analizador léxico en otro módulo
+      for _ <- 1..100 do
+        dice = Interpreter.eval_input("1d6 +3")
+        assert is_integer(dice)
+
+        assert dice >= 4 and dice <= 9
+      end
+    end
+
+    test "returns a rolled dice minus 3" do
+      # Uso del analizador léxico en otro módulo
+      for _ <- 1..100 do
+        dice = Interpreter.eval_input("1d6 - 3")
+        assert is_integer(dice)
+
+        assert dice >= -2 and dice <= 3
+      end
+    end
+
+    test "returns a rolled dice multiply 3" do
+      # Uso del analizador léxico en otro módulo
+      for _ <- 1..100 do
+        dice = Interpreter.eval_input("1d6 * 3")
+        assert is_integer(dice)
+        assert dice >= 3 and dice <= 18
+      end
+    end
+
+    test "returns a rolled dice div 3" do
+      # Uso del analizador léxico en otro módulo
+      for _ <- 1..100 do
+        dice = Interpreter.eval_input("1d6 / 3")
+
+        assert is_integer(dice)
+        assert dice >= 0 and dice <= 2
+      end
+    end
+
+    test "apply correctly order" do
+      # Uso del analizador léxico en otro módulo
+      number = Interpreter.eval_input("18 + 6 / 3")
+
+      assert is_integer(number)
+      assert number == 20
+    end
+
+    test "apply correctly parathesis" do
+      # Uso del analizador léxico en otro módulo
+      number = Interpreter.eval_input("18 / ((3 + 6) * 2)")
+
+      assert is_integer(number)
+      assert number == 1
     end
   end
 end
