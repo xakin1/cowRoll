1d277ab9a97eb0fc0d1214f9aad33bead3569aa1
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2023-11-22 20:33:46 +0100

    added all basic operation to the new structure

--- lib/cowRoll/parser/parser.ex
+++ lib/cowRoll/parser/parser.ex
@@ -5,79 +5,163 @@ defmodule Parser do
   # term   := factor * term | factor / term |factor
   # factor := ( expr ) | integer
   # integer    := 0 | 1 | 2 | ...
+  # boolean    := "true" | "false"
+
+  # general
+  lparen = ascii_char([?(]) |> label("(")
+  rparen = ascii_char([?)]) |> label(")")
 
   whitespace = choice([string(" "), string("\n"), string("\t")])
 
+  # arithmetic
   integer =
     repeat(ignore(whitespace))
     |> integer(min: 1)
+    |> tag(:number)
 
-  factor =
+  negation =
+    repeat(ignore(whitespace))
+    |> ignore(ascii_char([?-]))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:factor))
+    |> repeat(ignore(whitespace))
+    |> tag(:negation)
+
+  plus =
+    parsec(:term)
+    |> repeat(ignore(whitespace))
+    |> ignore(ascii_char([?+]))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:expr))
+    |> repeat(ignore(whitespace))
+    |> tag(:plus)
+
+  minus =
+    parsec(:term)
+    |> repeat(ignore(whitespace))
+    |> ignore(ascii_char([?-]))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:expr))
+    |> repeat(ignore(whitespace))
+    |> tag(:minus)
+
+  mult =
+    parsec(:factor)
+    |> repeat(ignore(whitespace))
+    |> ignore(ascii_char([?*]))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:term))
+    |> repeat(ignore(whitespace))
+    |> tag(:mult)
+
+  div =
+    parsec(:factor)
+    |> repeat(ignore(whitespace))
+    |> ignore(ascii_char([?/]))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:term))
+    |> repeat(ignore(whitespace))
+    |> tag(:div)
+
+  # boolean
+  true_ =
+    repeat(ignore(whitespace))
+    |> string("true")
+    |> replace(true)
+
+  false_ =
+    repeat(ignore(whitespace))
+    |> string("false")
+    |> replace(false)
+
+  boolean = choice([true_, false_]) |> tag(:boolean)
+
+  not_ =
+    repeat(ignore(whitespace))
+    |> ignore(string("not"))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:factor))
+    |> repeat(ignore(whitespace))
+    |> tag(:not)
+
+  and_ =
+    parsec(:factor)
+    |> repeat(ignore(whitespace))
+    |> ignore(string("and"))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:term))
+    |> repeat(ignore(whitespace))
+    |> tag(:and)
+
+  or_ =
+    parsec(:factor)
+    |> repeat(ignore(whitespace))
+    |> ignore(string("or"))
+    |> repeat(ignore(whitespace))
+    |> concat(parsec(:term))
+    |> repeat(ignore(whitespace))
+    |> tag(:or)
+
+  grouping = repeat(ignore(whitespace)) |> ignore(lparen) |> parsec(:expr) |> ignore(rparen)
+
+  # factor := ( expr ) | integer | boolean
+
+  defcombinatorp(
+    :factor,
     empty()
     |> choice(
       [
-        repeat(ignore(whitespace))
-        |> ignore(ascii_char([?(]))
-        |> repeat(ignore(whitespace))
-        |> concat(parsec(:expr))
-        |> repeat(ignore(whitespace))
-        |> ignore(ascii_char([?)])),
-        integer
+        grouping,
+        not_,
+        negation,
+        integer,
+        boolean
       ],
-      gen_weights: [1, 1]
+      gen_weights: [1, 1, 1, 2, 2]
     )
+  )
 
-  # Recursive definitions require using defparsec with the parsec combinator
+  # Termin := factor * term | factor / term | factor or term | factor and term | factor
 
   defcombinatorp(
     :term,
     empty()
     |> choice(
       [
-        factor
-        |> repeat(ignore(whitespace))
-        |> ignore(ascii_char([?*]))
-        |> repeat(ignore(whitespace))
-        |> concat(parsec(:term))
-        |> repeat(ignore(whitespace))
-        |> tag(:mult),
-        factor
-        |> repeat(ignore(whitespace))
-        |> ignore(ascii_char([?/]))
-        |> repeat(ignore(whitespace))
-        |> concat(parsec(:term))
-        |> repeat(ignore(whitespace))
-        |> tag(:div),
-        factor
+        mult,
+        div,
+        and_,
+        or_,
+        parsec(:factor)
       ],
-      gen_weights: [1, 1, 3]
+      gen_weights: [1, 1, 1, 1, 3]
     )
   )
+
+  # expr := term + expr | term - expr | term
 
   defcombinatorp(
     :expr,
     empty()
     |> choice(
       [
-        parsec(:term)
-        |> repeat(ignore(whitespace))
-        |> ignore(ascii_char([?+]))
-        |> repeat(ignore(whitespace))
-        |> concat(parsec(:expr))
-        |> repeat(ignore(whitespace))
-        |> tag(:plus),
-        parsec(:term)
-        |> repeat(ignore(whitespace))
-        |> ignore(ascii_char([?-]))
-        |> repeat(ignore(whitespace))
-        |> concat(parsec(:expr))
-        |> repeat(ignore(whitespace))
-        |> tag(:minus),
+        plus,
+        minus,
         parsec(:term)
       ],
       gen_weights: [1, 1, 3]
     )
   )
+
+  defp fold_infixl(acc) do
+    acc
+    |> Enum.reverse()
+    |> Enum.chunk_every(2)
+    |> List.foldr([], fn
+      [l], [] -> l
+      [r, op], l -> {op, [l, r]}
+    end)
+  end
 
   defparsec(:parse, parsec(:expr))
 end
--- test/cowRoll/Interpreter/parser/parser_test.exs
+++ test/cowRoll/Interpreter/parser/parser_test.exs
@@ -3,37 +3,60 @@ defmodule CowRoll.ParserTest do
   use ExUnit.Case
 
   describe "detect basic operations correctly" do
+    test "parse integer" do
+      # Uso del analizador léxico en otro módulo
+      input = "1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [number: [1]]
+
+      input = "1 "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [number: [1]]
+
+      input = " 1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [number: [1]]
+
+      input = " 1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [number: [1]]
+    end
+
     test "parse plus operation" do
       # Uso del analizador léxico en otro módulo
       input = "1+1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = "1+ 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = "1 +1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = "1 + 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = " 1 + 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = " 1 + 1 "
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
     end
 
     test "parse minus operation" do
@@ -41,32 +64,32 @@ defmodule CowRoll.ParserTest do
       input = "1-1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = "1- 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = "1 -1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = "1 - 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = " 1 - 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = " 1 - 1 "
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
     end
 
     test "parse mult operation" do
@@ -74,32 +97,32 @@ defmodule CowRoll.ParserTest do
       input = "1*1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
 
       input = "1* 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
 
       input = "1 *1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
 
       input = "1 * 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
 
       input = " 1 * 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
 
       input = " 1 * 1 "
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
     end
 
     test "parse div operation" do
@@ -107,32 +130,32 @@ defmodule CowRoll.ParserTest do
       input = "1/1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = "1/ 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = "1 /1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = "1 / 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = " 1 / 1"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = " 1 / 1 "
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
     end
 
     test "parse parenthesis" do
@@ -140,23 +163,145 @@ defmodule CowRoll.ParserTest do
       input = "(1/1)"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [div: [1, 1]]
+      assert token == [div: [number: [1], number: [1]]]
 
       input = "( 1+1)"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [plus: [1, 1]]
+      assert token == [plus: [number: [1], number: [1]]]
 
       input = "( 1-1 )"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [minus: [1, 1]]
+      assert token == [minus: [number: [1], number: [1]]]
 
       input = "(1 * 1 )"
       {_, token, _, _, _, _} = Parser.parse(input)
 
-      assert token == [mult: [1, 1]]
+      assert token == [mult: [number: [1], number: [1]]]
     end
+
+    test "parse boolean" do
+      # Uso del analizador léxico en otro módulo
+      input = "true"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [boolean: [true]]
+
+      input = "true "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [boolean: [true]]
+
+      input = " false"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [boolean: [false]]
+
+      input = " false "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [boolean: [false]]
+    end
+
+    test "parse and operation" do
+      # Uso del analizador léxico en otro módulo
+      input = "trueandtrue"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [and: [boolean: [true], boolean: [true]]]
+
+      input = "trueand false"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [and: [boolean: [true], boolean: [false]]]
+
+      input = "false andtrue"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [and: [boolean: [false], boolean: [true]]]
+
+      input = "false and false"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [and: [boolean: [false], boolean: [false]]]
+    end
+
+    test "parse or operation" do
+      # Uso del analizador léxico en otro módulo
+      input = "trueortrue"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [or: [boolean: [true], boolean: [true]]]
+
+      input = "trueor false"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [or: [boolean: [true], boolean: [false]]]
+
+      input = "false ortrue"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [or: [boolean: [false], boolean: [true]]]
+
+      input = "false or false"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [or: [boolean: [false], boolean: [false]]]
+    end
+
+    test "parse not operation" do
+      # Uso del analizador léxico en otro módulo
+      input = "not true"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [not: [boolean: [true]]]
+
+      input = "notfalse"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [not: [boolean: [false]]]
+
+      input = " nottrue"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [not: [boolean: [true]]]
+
+      input = "notfalse "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [not: [boolean: [false]]]
+    end
+
+    test "parse negation operation" do
+      input = "- 1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [negation: [number: [1]]]
+
+      input = "-1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [negation: [number: [1]]]
+
+      input = " - 1 "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [negation: [number: [1]]]
+
+      input = " - 1"
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [negation: [number: [1]]]
+
+      input = "- 1 "
+      {_, token, _, _, _, _} = Parser.parse(input)
+
+      assert token == [negation: [number: [1]]]
+    end
+  end
+
+  describe "detect conditionals correctly" do
   end
 
   describe "Arithmetic operations" do
