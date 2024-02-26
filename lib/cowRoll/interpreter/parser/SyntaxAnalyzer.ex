6c8568fcf5f94c32c4bd9f39cd85494853c78765
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2024-02-26 21:21:17 +0100

    improce syntaxAnalyzer

--- lib/cowRoll/interpreter/parser/SyntaxAnalyzer.ex
+++ lib/cowRoll/interpreter/parser/SyntaxAnalyzer.ex
@@ -1,105 +1,133 @@
 defmodule SyntaxAnalyzer do
   def analyze(tokens) do
-    count_parenthesis(tokens)
-    count_blocks(tokens)
+    analyze_aux(tokens, %{
+      open_parenthesis: 0,
+      close_parenthesis: 0,
+      open_blocks: 0,
+      close_blocks: 0,
+      left_missing_parenthesis: [],
+      right_missing_parenthesis: [],
+      start_missing_blocks: [],
+      end_missing_blocks: []
+    })
   end
 
-  defp count_parenthesis(tokens) do
-    count_parenthesis(tokens, 0, 0, [], [])
+  defp analyze_aux([], records) do
+    process_analyze(records)
   end
 
-  defp count_parenthesis([], open, close, left_missing, right_missing) do
-    process_parenthesis({open, close, left_missing, right_missing})
+  defp analyze_aux([{:"(", linea} | rest], records) do
+    records_updated =
+      Map.update!(records, :open_parenthesis, &(&1 + 1))
+      |> Map.update!(:left_missing_parenthesis, &[{linea, "("} | &1])
+
+    analyze_aux(rest, records_updated)
   end
 
-  defp count_parenthesis([{:"(", linea} | rest], open, close, left_missing, right_missing) do
-    count_parenthesis(rest, open + 1, close, left_missing, right_missing ++ [{linea, "("}])
+  defp analyze_aux([{:if, linea} | rest], records) do
+    records_updated =
+      Map.update!(records, :open_blocks, &(&1 + 1))
+      |> Map.update!(:start_missing_blocks, &[{linea, "if"} | &1])
+
+    analyze_aux(rest, records_updated)
   end
 
-  defp count_parenthesis([{:")", linea} | rest], open, close, left_missing, right_missing) do
-    if open > close do
-      count_parenthesis(rest, open, close + 1, left_missing, right_missing)
-    else
-      count_parenthesis(rest, open, close + 1, left_missing ++ [{linea, ")"}], right_missing)
-    end
+  defp analyze_aux([{:for, linea} | rest], records) do
+    records_updated =
+      Map.update!(records, :open_blocks, &(&1 + 1))
+      |> Map.update!(:start_missing_blocks, &[{linea, "for"} | &1])
+
+    analyze_aux(rest, records_updated)
   end
 
-  defp count_parenthesis([_ | rest], open, close, left_missing, right_missing) do
-    count_parenthesis(rest, open, close, left_missing, right_missing)
+  defp analyze_aux([{:end, linea} | rest], records) do
+    open_b = Map.get(records, :open_blocks, 0)
+    close_b = Map.get(records, :close_blocks, 0)
+
+    new_records =
+      if open_b > close_b do
+        Map.update!(records, :close_blocks, &(&1 + 1))
+      else
+        Map.update!(records, :close_blocks, &(&1 + 1))
+        |> Map.update!(:end_missing_blocks, &[{linea, "end"} | &1])
+      end
+
+    analyze_aux(rest, new_records)
   end
 
-  defp process_parenthesis({open, close, left_missing, right_missing}) do
-    if open == close do
+  defp analyze_aux([{:")", linea} | rest], records) do
+    open_p = Map.get(records, :open_parenthesis, 0)
+    close_p = Map.get(records, :close_parenthesis, 0)
+
+    new_records =
+      if open_p > close_p do
+        Map.update!(records, :close_parenthesis, &(&1 + 1))
+      else
+        Map.update!(records, :close_parenthesis, &(&1 + 1))
+        |> Map.update!(:right_missing_parenthesis, &[{linea, ")"} | &1])
+      end
+
+    analyze_aux(rest, new_records)
+  end
+
+  defp analyze_aux([_ | rest], records) do
+    analyze_aux(rest, records)
+  end
+
+  defp process_analyze(records) do
+    open_p = Map.get(records, :open_parenthesis, 0)
+    close_p = Map.get(records, :close_parenthesis, 0)
+
+    open_b = Map.get(records, :open_blocks, 0)
+    close_b = Map.get(records, :close_blocks, 0)
+
+    right_missing_parenthesis = Map.get(records, :right_missing_parenthesis, [])
+    left_missing_parenthesis = Map.get(records, :left_missing_parenthesis, [])
+
+    start_missing_blocks = Map.get(records, :start_missing_blocks, [])
+    end_missing_blocks = Map.get(records, :end_missing_blocks, [])
+
+    if open_p == close_p do
       :ok
     else
-      case {left_missing, right_missing} do
+      case {right_missing_parenthesis, left_missing_parenthesis} do
         {[], _} ->
           # Si estamos aquí, significa que falta un ')'
-          {line, _} = hd(right_missing)
+          {line, _} = hd(left_missing_parenthesis)
           raise RuntimeError, message: "Error: Missing ')' on line #{line}"
 
         {_, []} ->
           # Si estamos aquí, significa que falta un (
-          {line, _} = hd(left_missing)
+          {line, _} = hd(right_missing_parenthesis)
 
           raise RuntimeError, message: "Error: Missing '(' on line #{line}"
 
         _ ->
           # En este caso, tanto hay bloques de inicio como de fin faltantes, no podemos determinar cuál es el error principal.
-          {line, _} = hd(left_missing)
+          {line, _} = hd(left_missing_parenthesis)
           raise RuntimeError, message: "Error: Missing '(' on line #{line}"
       end
     end
-  end
 
-  defp count_blocks(tokens) do
-    count_blocks(tokens, 0, 0, [], [])
-  end
-
-  defp count_blocks([{:if, linea} | rest], starts, ends, start_missing, end_missing) do
-    count_blocks(rest, starts + 1, ends, start_missing, end_missing ++ [{linea, "if"}])
-  end
-
-  defp count_blocks([{:for, linea} | rest], starts, ends, start_missing, end_missing) do
-    count_blocks(rest, starts + 1, ends, start_missing, end_missing ++ [{linea, "for"}])
-  end
-
-  defp count_blocks([{:end, linea} | rest], starts, ends, start_missing, end_missing) do
-    if starts > ends do
-      count_blocks(rest, starts, ends + 1, start_missing, end_missing)
-    else
-      count_blocks(rest, starts, ends + 1, start_missing ++ [{linea, "end"}], end_missing)
-    end
-  end
-
-  defp count_blocks([_ | rest], starts, ends, start_missing, end_missing) do
-    count_blocks(rest, starts, ends, start_missing, end_missing)
-  end
-
-  defp count_blocks([], starts, ends, start_missing, end_missing) do
-    process_blocks({starts, ends, start_missing, end_missing})
-  end
-
-  defp process_blocks({starts, ends, start_missing, end_missing}) do
-    if starts == ends do
+    if open_b == close_b do
       :ok
     else
-      case {start_missing, end_missing} do
+      case {start_missing_blocks, end_missing_blocks} do
         {[], _} ->
           # Si estamos aquí, significa que falta un 'end'
-          {line, simbol} = hd(end_missing)
-          raise RuntimeError, message: "Error: Missing 'end' for '#{simbol}' on line #{line}"
+          {line, simbol} = hd(end_missing_blocks)
+          raise RuntimeError, message: "Error: Unexpected '#{simbol}' on line #{line}"
 
         {_, []} ->
           # Si estamos aquí, significa que falta un inicio correspondiente (como 'do', 'if', 'for', etc.)
-          {line, _} = hd(start_missing)
+          {line, simbol} = hd(start_missing_blocks)
 
           raise RuntimeError,
-            message: "Error: Unexpected 'end' on line #{line}"
+            message: "Error: Missing 'end' for '#{simbol}' on line #{line}"
 
         _ ->
           # En este caso, tanto hay bloques de inicio como de fin faltantes, no podemos determinar cuál es el error principal.
-          {line, simbol} = hd(end_missing)
+          {line, simbol} = hd(end_missing_blocks)
           raise RuntimeError, message: "Error: Missing 'end' for '#{simbol}' on line #{line}"
       end
     end
