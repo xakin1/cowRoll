ee8966db91f0c6b0c77e8fde2475cea46499f464
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2024-02-29 10:37:49 +0100

    remove unnecesary types

--- lib/cowRoll/utils/Types.ex
+++ lib/cowRoll/utils/Types.ex
@@ -18,20 +18,4 @@ defmodule TypesUtils do
   def get_type(value) when is_list(value) do
     "List"
   end
-
-  def get_type(value) when is_tuple(value) do
-    "Tuple"
-  end
-
-  def get_type(value) when is_float(value) do
-    "Float"
-  end
-
-  def get_type(value) when is_list(value) do
-    "List"
-  end
-
-  def get_type(value) when is_tuple(value) do
-    "Tuple"
-  end
 end
